eaa3283dcfd96ad0187fb808a6cce11273d1e3cc
ale2002solano <ale133valle@gmail.com>
2023-07-29 00:02:03 -0600

    Admin

--- modoAdministrativo/assetsAdmin/js/controladorAdmin.js
+++ modoAdministrativo/assetsAdmin/js/controladorAdmin.js
@@ -1,20 +1,31 @@
 function adminEmpresas(){
     document.getElementById("admin-empresa").style.display="block";
     document.getElementById("admin-producto-categoria").style.display="none";
+    document.getElementById("admin-ordenes").style.display="none";
 }
 function adminProductos(){
     document.getElementById("admin-producto-categoria").style.display="block";
     document.getElementById("admin-empresa").style.display="none";
     document.getElementById("productos").style.display="none";
+    document.getElementById("categorias-productos").style.display="none";
+    document.getElementById("admin-ordenes").style.display="none";
 }
 function adminMotoristas(){}
-function adminOrdenes(){}
+function adminOrdenes(){
+    document.getElementById("admin-ordenes").style.display="block";
+    document.getElementById("admin-producto-categoria").style.display="none";
+    document.getElementById("admin-empresa").style.display="none";
+}
+
+function empresasProductos(){
+    document.getElementById("categorias-productos").style.display="block";
+}
 
 function camisasProductos(){
     document.getElementById("camisa").style.display="block";
     document.getElementById("pantalones").style.display="none";
-    // document.getElementById("accesorios").style.display="none";
-    // document.getElementById("zapatos").style.display="none";
+    document.getElementById("accesorios").style.display="none";
+    document.getElementById("zapatos").style.display="none";
     document.getElementById("productos").style.display="block";
 }
 function pantalonesProductos(){
@@ -37,4 +48,7 @@ function zapatosProductos(){
     document.getElementById("camisa").style.display="none";
     document.getElementById("accesorios").style.display="none";
     document.getElementById("productos").style.display="block";
+}
+function formatoOrdenes(){
+    document.getElementById("formato").style.display="block";
 }
