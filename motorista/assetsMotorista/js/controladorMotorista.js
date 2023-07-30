ee5bcac7b87996053616c408059d75d3a2954504
ale2002solano <ale133valle@gmail.com>
2023-07-30 13:13:23 -0600

    MOTORISTA CAMBIOS

---
+++ motorista/assetsMotorista/js/controladorMotorista.js
@@ -0,0 +1,8 @@
+function registro(){
+    document.getElementById("registros").style.display="block";
+    document.getElementById("login").style.display="none";
+}
+function irLogin(){
+    document.getElementById("login").style.display="block";
+    document.getElementById("registros").style.display="none";
+}
