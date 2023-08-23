d9f82f45a51badf029c6df5b80287b71a7a78a1d
ale2002solano <ale133valle@gmail.com>
2023-08-23 07:54:01 -0600

    productos schema y model

--- backend/src/models/productos.model.ts
+++ backend/src/models/productos.model.ts
@@ -1,1 +1,9 @@
+import mongoose from "mongoose";
 
+export interface Productos {
+    _id?: mongoose.Types.ObjectId;
+    idProductos:String;
+    img: string;
+    nombreProducto: string;
+    precio: string;
+}
